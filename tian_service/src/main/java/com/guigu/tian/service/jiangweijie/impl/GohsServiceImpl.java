a1fde5eb717fd371fa519cb44b3f80ccae5b08a3
jinagjiangssm <2086863823@qq.com>
2021-05-06 15:11:04 +0800

    蒋伟杰

--- tian_service/src/main/java/com/guigu/tian/service/jiangweijie/impl/GohsServiceImpl.java
+++ tian_service/src/main/java/com/guigu/tian/service/jiangweijie/impl/GohsServiceImpl.java
@@ -1,12 +1,45 @@
 package com.guigu.tian.service.jiangweijie.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.guigu.tian.entity.Gongyingshang;
 import com.guigu.tian.entity.Gongyingshop;
 import com.guigu.tian.mapper.jiangweijie.GhshanMapper;
 import com.guigu.tian.service.jiangweijie.GohsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GohsServiceImpl extends ServiceImpl<GhshanMapper, Gongyingshop> implements GohsService {
+
+    @Autowired
+    GhshanMapper ghshanMapper;
+
+
+    @Override
+    public PageInfo<Gongyingshop> selectList(String name,int pageNo, int pageSize) {
+        PageHelper.startPage(pageNo,pageSize);
+        List<Gongyingshop> gongyingshops = ghshanMapper.selectList(name);
+        PageInfo<Gongyingshop> pageInfo=new PageInfo<>(gongyingshops);
+        return pageInfo;
+    }
+
+    @Override
+    public int gonshsup(Gongyingshop shop) {
+        return ghshanMapper.Ghshanupdate(shop);
+    }
+
+    @Override
+    public int goninsert(Gongyingshop shop) {
+
+        return ghshanMapper.Ghshaninsert(shop);
+    }
+
+    @Override
+    public int gondelete(int id) {
+        return ghshanMapper.Ghshangchu(id);
+    }
 }
