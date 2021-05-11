44a542145e3a7adc3a56ee38f636c7dae30d0781
jinagjiangssm <2086863823@qq.com>
2021-05-11 18:59:34 +0800

    蒋伟杰

--- tian_web/src/main/java/com/guigu/tian/controller/xiaosaiz/XszUserController.java
+++ tian_web/src/main/java/com/guigu/tian/controller/xiaosaiz/XszUserController.java
@@ -3,11 +3,14 @@ package com.guigu.tian.controller.xiaosaiz;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.guigu.tian.entity.User;
 import com.guigu.tian.service.xiaosaiz.XszUserService;
+import org.apache.shiro.crypto.hash.Md5Hash;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @RequestMapping("xszuser")
 @Controller
@@ -34,7 +37,18 @@ private XszUserService userService;
         QueryWrapper<User> queryWrapper=new QueryWrapper<User>();
         queryWrapper.eq("uname",user.getUname());
 
+        List<User> list = userService.list(queryWrapper);
 
+        System.out.println(list.size());
+
+        if(list.size()==0){//判断是否有该用户
+            Md5Hash md5=new Md5Hash(user.getUpawssword(),"555"); //加密
+            String s = md5.toString();
+            user.setUpawssword(s);
+
+            userService.save(user); //添加用户
+            return true;
+        }
 
         return  false;
 
